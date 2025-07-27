99d41555275b33ef3d53cb44e0ad0a646d0262cd
luizribeiro <ludehenriq@gmail.com>
2025-07-27 18:50:43 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"github.com/Lzrb0x/study-go-api.git/config"
 	"github.com/Lzrb0x/study-go-api.git/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization failed: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
